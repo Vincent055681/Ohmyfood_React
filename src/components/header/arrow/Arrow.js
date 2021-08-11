f185de863c0655b3d8aba533311a7c68df201ccf
vincent <to@toto.fr>
2021-08-11 11:15:43 +0200

    Mise à jour du lien de la flèche dans le header

--- src/components/header/arrow/Arrow.js
+++ src/components/header/arrow/Arrow.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Arrow.scss";
+import { Link } from "react-router-dom";
+
 
 const Arrow = (props) => {
     return (
         <div className="header__arrow-back">
-            <a href="../index.html">
+            <Link to="/">
                 <i className="fas fa-arrow-left"></i>
-            </a>
+            </Link>
         </div>
     );
 };
